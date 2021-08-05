b753912c367a035029349c315d2bcf32791c10fc
Jhonatan <jhonatanfnm@gmail.com>
2021-08-05 16:33:36 -0300

    Aula 619. Validando Usuário Administrador

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,3 +1,4 @@
+const admin = require('./admin')
 module.exports = app => {
     app.post('/signup', app.api.user.save)
     app.post('/signin', app.api.auth.signin)
@@ -5,18 +6,18 @@ module.exports = app => {
 
     app.route('/users')
         .all(app.config.passport.authenticate())
-        .post(app.api.user.save)
-        .get(app.api.user.get)
+        .post(admin(app.api.user.save))
+        .get(admin(app.api.user.get))
 
     app.route('/users/:id')
         .all(app.config.passport.authenticate())
-        .put(app.api.user.save)
-        .get(app.api.user.getById)
+        .put(admin(app.api.user.save))
+        .get(admin(app.api.user.getById))
 
     app.route('/categories')
         .all(app.config.passport.authenticate())
-        .post(app.api.category.save)
-        .get(app.api.category.get)
+        .post(admin(app.api.category.save))
+        .get(admin(app.api.category.get))
 
     // Cuidado com a ordem das rotas, as rotas mais específicas tem que vir sempre antes
     app.route('/categories/tree')
@@ -25,14 +26,14 @@ module.exports = app => {
 
     app.route('/categories/:id')
         .all(app.config.passport.authenticate())
-        .put(app.api.category.save)
+        .put(admin(app.api.category.save))
         .get(app.api.category.getById)
-        .delete(app.api.category.remove)
+        .delete(admin(app.api.category.remove))
 
     app.route('/articles')
         .all(app.config.passport.authenticate())
-        .post(app.api.article.save)
-        .get(app.api.article.get)
+        .post(admin(app.api.article.save))
+        .get(admin(app.api.article.get))
 
     app.route('/categories/:id/articles')
         .all(app.config.passport.authenticate())
@@ -40,8 +41,8 @@ module.exports = app => {
 
     app.route('/articles/:id')
         .all(app.config.passport.authenticate())
-        .put(app.api.article.save)
+        .put(admin(app.api.article.save))
         .get(app.api.article.getById)
-        .delete(app.api.article.remove)
+        .delete(admin(app.api.article.remove))
 
 }
