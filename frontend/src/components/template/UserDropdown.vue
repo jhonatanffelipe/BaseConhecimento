5be958a2200a87409cd38d80a608c5f06f559d52
Jhonatan <jhonatanfnm@gmail.com>
2021-08-06 18:22:40 -0300

    Aula 630. Router

--- frontend/src/components/template/UserDropdown.vue
+++ frontend/src/components/template/UserDropdown.vue
@@ -9,7 +9,9 @@
     </div>
 
     <div class="user-dropdown-content">
-      <a href=""><i class="fa fa-cogs"></i>Administração</a>
+      <router-link to="/admin">
+        <i class="fa fa-cogs"></i>Administração
+      </router-link>
       <a href=""><i class="fa fa-sign-out"></i>Sair</a>
     </div>
   </div>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 import App from './App.vue'
 import store from './config/store'
+import router from './config/router'
 
 import './config/bootstrap'
 
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: h => h(App),
 }).$mount('#app')
