096caa605d0635b526f7109755604afc968595af
Jhonatan <jhonatanfnm@gmail.com>
2021-08-06 09:35:59 -0300

    Aula 626. Componente Cabeçalho

--- frontend/src/components/template/Header.vue
+++ frontend/src/components/template/Header.vue
@@ -1,10 +1,29 @@
 <template>
-  <header class="header"></header>
+  <header class="header">
+    <a class="toggle" @click="toglleMenu" v-if="!hideToggle">
+      <i class="fa fa-lg" :class="icon"></i>
+    </a>
+    <h1 class="title">{{ title }}</h1>
+  </header>
 </template>
 
 <script>
 export default {
   name: "Header",
+  props: {
+    title: String,
+    hideToggle: Boolean,
+  },
+  computed: {
+    icon() {
+      return "fa-angle-left";
+    },
+  },
+  methods: {
+    toglleMenu() {
+      console.log("Clicou.");
+    },
+  },
 };
 </script>
 
@@ -16,5 +35,34 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.title {
+  font-size: 1.2em;
+  color: #fff;
+  font-weight: 100;
+  flex-grow: 1;
+  text-align: center;
+}
+
+.title a {
+  color: #fff;
+  text-decoration: none;
+}
+
+header.header > a.toggle {
+  width: 60px;
+  height: 100%;
+  color: #fff;
+  justify-self: flex-start;
+  text-decoration: none;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header.header > a.toggle:hover {
+  background-color: #0003;
 }
 </style>
