c85ae0a5a2f6ed38c79f02f70d6ae168753ea073
Jhonatan <jhonatanfnm@gmail.com>
2021-08-07 15:57:39 -0300

    Aul 635. Cadastro de Usuário #02

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -1,12 +1,80 @@
 <template>
   <div class="user-admin">
+    <b-form>
+      <input id="user-id" type="hidden" v-model="user.id" />
+      <b-row>
+        <b-col md="6" sm="12">
+          <b-form-group label="Nome:" lobel-for="user-name">
+            <b-form-input
+              id="user-name"
+              type="text"
+              v-model="user.name"
+              required
+              placeholder="Informe o Nome do usuário"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col md="6" sm="12">
+          <b-form-group label="E-mail:" lobel-for="user-email">
+            <b-form-input
+              id="user-email"
+              type="email"
+              v-model="user.email"
+              required
+              placeholder="Informe o E-mail do usuário"
+            />
+          </b-form-group>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col md="6" sm="12">
+          <b-form-group label="Senha:" lobel-for="user-password">
+            <b-form-input
+              id="user-password"
+              type="password"
+              v-model="user.password"
+              required
+              placeholder="Informe a Senha do usuário"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col md="6" sm="12">
+          <b-form-group
+            label="Confirmação de Senha:"
+            lobel-for="user-confirmPassword"
+          >
+            <b-form-input
+              id="user-confirmPassword"
+              type="password"
+              v-model="user.confirmPassword"
+              required
+              placeholder="Confirme a senha do usuário"
+            />
+          </b-form-group>
+        </b-col>
+      </b-row>
+
+      <b-form-checkbox id="user-admin" v-model="user.admin" class="mt-3 mb-3">
+        Administrador?
+      </b-form-checkbox>
+
+      <b-button variant="primary" v-if="mode === 'save'" @click="save">
+        Salvar
+      </b-button>
+      <b-button variant="danger" v-if="mode === 'remove'" @click="remove">
+        Excluir
+      </b-button>
+      <b-button class="ml-2" @click="reset"> Cancelar </b-button>
+    </b-form>
+
     <b-table hover stiped :items="users" :fields="fields"> </b-table>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import { baseUrl } from "@/global";
+import { baseUrl, showError } from "@/global";
 
 export default {
   name: "UserAdmin",
@@ -35,6 +103,32 @@ export default {
       axios.get(url).then((response) => {
         this.users = response.data;
       });
+    },
+    reset() {
+      this.mode = "save";
+      this.user = {};
+      this.loadUsers();
+    },
+    save() {
+      const method = this.user.id ? "put" : "post";
+      const id = this.user.id ? `/${this.user.id}` : "";
+
+      axios[method](`${baseUrl}/users${id}`, this.user)
+        .then(() => {
+          this.$toasted.global.defaultSuccess();
+          this.reset();
+        })
+        .catch(showError);
+    },
+    remove() {
+      const id = this.user.id;
+      axios
+        .delete(`${baseUrl}/users/${id}`)
+        .then(() => {
+          this.$toasted.global.defaultSuccess();
+          this.reset();
+        })
+        .cath(showError);
     },
   },
   mounted() {
