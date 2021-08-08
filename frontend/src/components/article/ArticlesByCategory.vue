d2226a60b660bda2eb27659289ef9a68986f7f78
Jhonatan <jhonatanfnm@gmail.com>
2021-08-08 19:05:42 -0300

    Aula 642. Artigos Por Categoria #02

--- frontend/src/components/article/ArticlesByCategory.vue
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="articles-by-category">
     <PageTitle icon="fa fa-folder" :main="category.name" sub="Categoria" />
+
+    <ul>
+      <li v-for="article in articles" :key="article.id">
+        {{ article.name }}
+      </li>
+    </ul>
+    <div class="load-more">
+      <button
+        v-if="loadMore === true"
+        class="btn btn-lg btn-primary"
+        @click="getArticles"
+      >
+        Carregar Mais Artigos
+      </button>
+    </div>
   </div>
 </template>
 
@@ -27,14 +42,36 @@ export default {
         this.category = response.data;
       });
     },
+    getArticles() {
+      const url = `${baseUrl}/categories/${this.category.id}/articles?page=${this.page}`;
+      axios(url).then((response) => {
+        this.articles = this.articles.concat(response.data);
+        this.page++;
+
+        if (response.data.length === 0) {
+          this.loadMore = false;
+        }
+      });
+    },
   },
   mounted() {
     this.category.id = this.$route.params.id;
     this.getCategory();
-    console.log();
+    this.getArticles();
   },
 };
 </script>
 
 <style>
+.articles-by-category ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.articles-by-category .load-more {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
+}
 </style>
