3076c874baf8950fc0a14730c9cde2bd75f25ae1
Jhonatan <jhonatanfnm@gmail.com>
2021-08-08 17:07:02 -0300

    Aula 641. Artigos Por Categoria #01

---
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="articles-by-category">
+    <PageTitle icon="fa fa-folder" :main="category.name" sub="Categoria" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { baseUrl } from "@/global";
+import PageTitle from "@/components/template/PageTitle";
+
+export default {
+  name: "ArticlesByCategory",
+  components: { PageTitle },
+  data: function () {
+    return {
+      category: {},
+      articles: [],
+      page: 1,
+      loadMore: true,
+    };
+  },
+  methods: {
+    getCategory() {
+      const url = `${baseUrl}/categories/${this.category.id}`;
+      axios(url).then((response) => {
+        this.category = response.data;
+      });
+    },
+  },
+  mounted() {
+    this.category.id = this.$route.params.id;
+    this.getCategory();
+    console.log();
+  },
+};
+</script>
+
+<style>
+</style>
