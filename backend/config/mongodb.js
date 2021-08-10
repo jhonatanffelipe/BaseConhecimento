bfbae2f0a541ffe54a90dcacbbc93c24c0f8fa9f
Jhonatan <jhonatanfnm@gmail.com>
2021-08-10 18:06:50 -0300

    Adicionando assets do readme

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/base_conhecimento', { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect('mongodb://172.17.0.3/base_conhecimento', { useUnifiedTopology: true, useNewUrlParser: true })
     .catch(e => {
         console.log(`❌ Erro: Não foi passivel conectar com o mongodb! ${e}`);
     })
